6e85c2902dc3a180c46c07578416566345f91fa0
mai nguyen <maing528@hotmail.com>
2021-08-05 14:20:21 -0700

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by mai ng on 8/5/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
