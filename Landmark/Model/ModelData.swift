2be183865a9db1892b816135f7b438c490449959
mai nguyen <maing528@hotmail.com>
2021-08-11 13:39:19 -0700

    Complete the Category View

--- Landmark/Model/ModelData.swift
+++ Landmark/Model/ModelData.swift
@@ -11,6 +11,10 @@ import Foundation
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
+    
     var categories: [String: [Landmark]] {
         Dictionary(
             grouping: landmarks) { $0.category.rawValue
