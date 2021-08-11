664c5974c547a3b3c5a132a7e1fd7069dc8fa172
mai nguyen <maing528@hotmail.com>
2021-08-11 11:50:10 -0700

    Combine the Badge Foreground and Background

---
+++ Landmark/Badges/Badge.swift
@@ -0,0 +1,41 @@
+//
+//  Badge.swift
+//  Landmark
+//
+//  Created by mai ng on 8/11/21.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    static let rotationCount = 8
+    
+    
+    var badgeSymbols: some View {
+        //Add a ForEach view to rotate and display copies of the badge symbol
+        ForEach(0..<Badge.rotationCount) { i in
+            RotatedBadgeSymbol(angle: .degrees(Double(i) / Double(Badge.rotationCount)) * 360.0
+            )
+        }
+        .opacity(0.5)
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            
+            GeometryReader { geometry in
+                badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+                }
+        
+            }
+        .scaledToFit()
+        }
+    }
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
